1de714370049a03b3381510d0577bdd80cb2d713
M.Sultan <msultan.74u@gmail.com>
2025-11-27 14:19:20 +0200

    create notes app

---
+++ lib/Pages/Home_Page.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  static String id = 'home_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
